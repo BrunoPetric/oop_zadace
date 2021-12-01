ca9e11a572c0ae7a8b4feaebcb9625c2828c7295
BrunoPetric <93745263+BrunoPetric@users.noreply.github.com>
2021-12-01 01:28:00 +0100

    Add files via upload

---
+++ DZ2/Zadaca2/classlibrary/ForecastUtilities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace classlibrary
+{
+    public class ForecastUtilities
+    {
+        public static Weather FindWeatherWithLargestWindchill(Weather[] weathers)
+        {
+            Weather maxObj = weathers[0];
+            double maxx = weathers[0].CalculateWindChill();
+
+            foreach (Weather weather in weathers)
+            {
+                if (weather.CalculateWindChill() > maxx)
+                {
+                    maxx = weather.CalculateWindChill();
+                    maxObj = weather;
+                }
+            }
+            return maxObj;
+        }
+        public static DailyForecast Parse(string parse)
+        {
+            string datum = parse.Substring(0,19);
+            string vrijeme = parse.Substring(20);
+            var dateString = datum;
+            DateTime date1 = Convert.ToDateTime(datum);//DateTime.Parse(dateString, System.Globalization.CultureInfo.CurrentCulture);
+            string[] weathers = vrijeme.Split(',');
+            weathers[0] = weathers[0].Replace('.',',');
+            weathers[1] = weathers[1].Replace('.', ',');
+            weathers[2] = weathers[2].Replace('.', ',');
+            Weather weather = new Weather(Convert.ToDouble(weathers[0]), Convert.ToDouble(weathers[2]), Convert.ToDouble(weathers[1]));
+
+            return new DailyForecast(date1, weather);
+        }
+    }
+}
