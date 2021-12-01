6a8992083750dcc507e24f96018e1534577ef327
BrunoPetric <93745263+BrunoPetric@users.noreply.github.com>
2021-12-01 09:00:14 +0100

    Add files via upload

---
+++ DZ2/Zadaca2/classlibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace classlibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
