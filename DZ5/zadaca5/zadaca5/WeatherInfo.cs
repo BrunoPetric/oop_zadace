5488c30a2222e286b4050eeae035d536d29dab8f
BrunoPetric <93745263+BrunoPetric@users.noreply.github.com>
2022-01-28 22:37:37 +0100

    Update WeatherInfo.cs

--- DZ5/zadaca5/zadaca5/WeatherInfo.cs
+++ DZ5/zadaca5/zadaca5/WeatherInfo.cs
@@ -12,24 +12,6 @@ namespace zadaca5
         {
             public double lon { get; set; }
             public double lat { get; set; }
-        }
-     //   public class weather
-     //   {
-     //       public string main { get; set; }
-     //       public string desc { get; set; }
-     //       public string icon { get; set; }
-     //   }
-     //   public class main
-     //   {
-     //       public double temp { get; set; }
-     //       
-     //   }
-
-    //   public class forecast
-    //   {
-    //       
-    //      // public List<weather> weather { get; set; }
-    //      // public main main { get; set; }
-    //   }
+        }   
     
 }
